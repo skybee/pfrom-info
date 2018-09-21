5c163d372793219750c7ac46a615d6c86235315a
Sb NBook <skybee14@gmail.com>
2018-09-21 16:42:56 +0300

    add trim to description

--- application/views/component/meta_og_v.php
+++ application/views/component/meta_og_v.php
@@ -3,12 +3,12 @@
 <meta property="og:title" content="<?=$doc_data['title']?>"/>
 <meta property="og:type" content="article"/>
 <meta property="og:url" content="http://<?=$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']?>"/>
-<meta property="og:site_name" content="СМИ Express"/>
-<meta property="og:description" content="<?= mb_substr( strip_tags($doc_data['text']), 0, 200)?>..."/>
+<meta property="og:site_name" content="PressFrom - <?= mb_strtoupper(LANG_CODE); ?>"/>
+<meta property="og:description" content="<?= trim(mb_substr( strip_tags($doc_data['text']), 0, 200))?>..."/>
 
 <?php if( !empty($doc_data['main_img']) ): ?>
 <!--medium--><meta property="og:image" content="http://<?=$_SERVER['HTTP_HOST']?>/upload/images/real/<?=$doc_data['main_img']?>"/>
 <?php endif; ?>
 
-<meta name="description" content="<?=$doc_data['title']?>: <?= mb_substr( strip_tags($doc_data['text']), 0, 100)?>..." />    
+<meta name="description" content="<?=$doc_data['title']?>: <?= trim(mb_substr( strip_tags($doc_data['text']), 0, 100))?>..." />    
 
