2fe88f3c4873aca600f51e17f7d0cadc85063ee8
Sb NBook <skybee14@gmail.com>
2018-10-23 11:37:28 +0300

    google jquery source

--- application/views/main_v.php
+++ application/views/main_v.php
@@ -34,8 +34,10 @@
             <meta name="robots" content="noindex, follow" />
         <?php endif; ?>   
 
-        <script type='text/javascript' src='/js/skin1/jquery-1.11.3.min.js'></script>
-        <script type='text/javascript' src='/js/skin1/jquery-ui.min-tabs.js'></script>
+<!--        <script type='text/javascript' src='/js/skin1/jquery-1.11.3.min.js'></script>
+        <script type='text/javascript' src='/js/skin1/jquery-ui.min-tabs.js'></script>-->
+        <script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js'></script>
+        <script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js'></script>
         <script type='text/javascript' src='/js/skin1/jquery.magnific-popup.min.js'></script>
         <script type='text/javascript' src='/js/skin1/jquery.bxslider.min.js'></script>
         <script type='text/javascript' src='/js/skin1/gads.js?v=<?=js_version('/js/skin1/gads.js')?>'></script>
