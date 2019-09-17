022570d44fc631542d336130bb50bdb7951b26a4
Alex <Alex@LAPTOP>
2019-09-17 13:37:04 +0300

    UPD

--- application/controllers/ajax/Background.php
+++ application/controllers/ajax/Background.php
@@ -11,36 +11,29 @@ class Background extends CI_Controller{
     }
     
     function set_top(){
-        if( $_SERVER["REQUEST_METHOD"] != 'POST' ) exit('! Wrong Method');
         
         $id     = (int) $_POST['docId'];
-        $ip     = $_SERVER['HTTP_X_REAL_IP'];
+//        $ip     = $_SERVER['HTTP_X_REAL_IP'];
+        $ip     = $_SERVER['REMOTE_ADDR'];
         $ref    = $_POST['ref'];
         $rank   = 1;
         
-        if( preg_match("#^66\.249\.\d{1,3}\.\d{1,3}#i", $ip) ) exit('Lock IP');
+        if($_SERVER["REQUEST_METHOD"] != 'POST')                                exit('! Wrong Method');
+        if($id <= 1)                                                            exit('! Wrong Post ID');
+        if(filter_var($ip, FILTER_VALIDATE_IP) === false)                       exit('! Wrong IP');
         
-        if( preg_match("#(google|yandex|mail.ru|Nigma|Rambler|Ukr.net|Bing)#i", $ref) ){
+        if( preg_match("#^66\.249\.\d{1,3}\.\d{1,3}#i", $ip) )                  exit('Lock IP'); //GoogleBot
+        if( preg_match("#^(141\.8\.142|178\.154\.171)\.\d{1,3}#i", $ip) )       exit('Lock IP'); //YandexBot
+        if( preg_match("#^109\.239\.235\.\d{1,3}#i", $ip) )                     exit('Lock IP'); //May be GoogleBot
+        
+        if( preg_match("#(google|yandex|bing|yahoo|duckduckgo|mail.ru|Nigma|Rambler|Ukr.net)#i", $ref) ){
             $rank = 3;
-//            $this->steSearchQuery( $id, $ref );
         }
+        
+        $log_str = "ID: {$id} - IP: {$ip} - {$ref}\n";
         
         $this->article_m->set_article_rank($id, $ip, $rank);
     }
-    
-//    private function steSearchQuery( $id, $ref ){
-//        
-//        echo $id."\n".$ref;
-//        
-//        if( preg_match("#yandex#i", $ref) ){
-//            $refAr      = parse_url($ref);
-//            parse_str($refAr['query'], $refUriAr);
-//            print_r($refAr);
-//            print_r($refUriAr);
-//            
-//            echo base64_decode($refUriAr['etext']);
-//        }
-//    }
     
     function get_right_hc(){        
         if( isset($_COOKIE['country']) && mb_strlen($_COOKIE['country']) == 2  ){
