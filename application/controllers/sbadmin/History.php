b0f1310acba43fe56d45c7e1308bbcd2392bc59f
Alex <Alex@LAPTOP>
2019-09-24 14:34:23 +0300

    HistoryLoad News Graaph

---
+++ application/controllers/sbadmin/History.php
@@ -0,0 +1,26 @@
+<?php
+
+// URL: /ru/sbadmin/history/show_graph/3/
+
+class History extends CI_Controller{
+    
+    
+    function __construct() {
+        parent::__construct();
+        
+        $this->load->database();
+        $this->load->model('sbadmin/history_m');
+    }
+    
+//    function index(){ echo "index"; }
+    
+    function show_graph($month=3)
+    {
+        $month = (int) $month;
+        
+        $dataDateAr = $this->history_m->getPeriodHistory($month);
+        $viewData['dateDataAr'] = $dataDateAr;
+//        print_r($dataDateAr);
+        $this->load->view('sbadmin/index_v',$viewData);
+    }
+}
